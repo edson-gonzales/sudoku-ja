e93a495d3dfbdefca8b53b8a05f8299364d34bdc
Alejandrita <alejan_dra01@outlookcom>
2016-05-24 19:57:41 -0400

    Added Cell and SudokuBoard classes

---
+++ src/com/sudoku/Cell.java
@@ -0,0 +1,66 @@
+package com.sudoku;
+
+/**
+ * This class represent a Cell object
+ *
+ * @author Alejandra Arteaga
+ * @author Jose Cabrera
+ */
+public class Cell {
+    private int value;
+    private int posX = 0;
+    private int posY = 0;
+
+    public Cell(int x, int y, int value) {
+        this.posX = x;
+        this.posY = y;
+        this.value = value;
+    }
+
+    public Cell(int x, int y) {
+        this.posX = x;
+        this.posY = y;
+        this.value = 0;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getPosX() {
+        return this.posX;
+    }
+
+    public int getPosY() {
+        return this.posY;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    /**
+     * Verify if the cell is empty
+     *
+     * @return The condition of the cell if it has a zero value
+     */
+    public boolean isCellEmpty() {
+        if (this.value == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "PosX: " + getPosX() + " PosY: " + getPosY() + " Value: " + getValue();
+    }
+}
