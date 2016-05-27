7fd5894f806b552bcd9a8b2f3f4be203282a4827
alejandrita <alejan_dra01@outlook.com>
2016-05-26 23:35:50 -0400

    Updated

--- src/com/algorithm/Algorithm.java
+++ src/com/algorithm/Algorithm.java
@@ -3,8 +3,15 @@ package com.algorithm;
 import com.sudoku.SudokuBoard;
 
 /**
- * Created by Alejandra on 17/05/2016.
+ * This class represent the Algorithm interface
  */
 public interface Algorithm {
+
+    /**
+     * Solve a sudoku board game
+     *
+     * @param sudokuBoard The sudoku board of cells
+     * @return The condition of game result
+     */
     public Boolean solve(SudokuBoard sudokuBoard);
 }
--- src/com/algorithm/Backtracking.java
+++ src/com/algorithm/Backtracking.java
@@ -3,14 +3,21 @@ package com.algorithm;
 import com.sudoku.Cell;
 import com.sudoku.SudokuBoard;
 
-
 /**
  * This class represent the Backtracking algorithm
  */
 public class Backtracking implements Algorithm {
+    private SudokuBoard sudokuBoard;
 
+    /**
+     * Solve a sudoku board game
+     *
+     * @param sudokuBoard The sudoku board of cells
+     * @return The condition of game result
+     */
     @Override
     public Boolean solve(SudokuBoard sudokuBoard) {
+        this.sudokuBoard = sudokuBoard;
         Cell emptyCell = sudokuBoard.getFirstEmptyCell();
 
         if (emptyCell == null) {
@@ -21,7 +28,7 @@ public class Backtracking implements Algorithm {
         int column = emptyCell.getPosY();
 
         for (int num = 1; num <= 9; num++) {
-            if (isPracticable(row, column, sudokuBoard, num)) {
+            if (isPracticable(row, column, num)) {
                 sudokuBoard.setCell(row, column, num);
                 if (solve(sudokuBoard)) {
                     return true;
@@ -32,10 +39,18 @@ public class Backtracking implements Algorithm {
         return false;
     }
 
-    public Boolean isPracticable(int row, int column, SudokuBoard sudokuBoard, int num) {
-        Boolean isPracticableInColumn = !sudokuBoard.isUsedInColumn(column, num);
-        Boolean isPracticableInRow = !sudokuBoard.isUsedInRow(row, num);
-        Boolean isPracticableInSubGrid = !sudokuBoard.isUsedInSubGrid(row, column, num);
+    /**
+     * Verify if a num is practicable to set in a cell on the board
+     *
+     * @param row    The row position in the board
+     * @param column The column position in the board
+     * @param num    A digit number
+     * @return Return true if the num is practicable in row, column and sub grid
+     */
+    public Boolean isPracticable(int row, int column, int num) {
+        Boolean isPracticableInColumn = !this.sudokuBoard.isUsedInColumn(column, num);
+        Boolean isPracticableInRow = !this.sudokuBoard.isUsedInRow(row, num);
+        Boolean isPracticableInSubGrid = !this.sudokuBoard.isUsedInSubGrid(row, column, num);
         return isPracticableInColumn && isPracticableInRow && isPracticableInSubGrid;
     }
 }
