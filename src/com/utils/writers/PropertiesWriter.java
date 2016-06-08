5b33303dbfb6ad0c052358d4285515550539549f
josecabrera92 <jose.cabrera@jalasoft.com>
2016-06-08 19:05:36 -0400

    Updated variables in Config Properties

--- src/com/utils/writers/PropertiesWriter.java
+++ src/com/utils/writers/PropertiesWriter.java
@@ -21,13 +21,29 @@ public class PropertiesWriter {
     public static final String OUTPUT_FILE_NAME = "OUTPUT_FILE_NAME";
     public static final String LEVEL = "LEVEL";
     public static final String CHARACTER = "CHARACTER";
+    public static final String EASY_MIN = "EASY_MIN";
+    public static final String MEDIUM_MIN = "MEDIUM_MIN";
+    public static final String HARD_MIN = "HARD_MIN";
+    public static final String CUSTOM_MIN = "CUSTOM_MIN";
+    public static final String EASY_MAX = "EASY_MAX";
+    public static final String MEDIUM_MAX = "MEDIUM_MAX";
+    public static final String HARD_MAX = "HARD_MAX";
+    public static final String CUSTOM_MAX = "CUSTOM_MAX";
 
     public enum CONFIG {
         ALGORITHM("BackTracking"),
         OUTPUT_PATH("./"),
         OUTPUT_FILE_NAME("defaultName"),
         LEVEL("Easy"),
-        CHARACTER(".");
+        CHARACTER("."),
+        EASY_MIN("1"),
+        MEDIUM_MIN("3"),
+        HARD_MIN("6"),
+        CUSTOM_MIN(""),
+        EASY_MAX("3"),
+        MEDIUM_MAX("5"),
+        HARD_MAX("9"),
+        CUSTOM_MAX("");
 
         private String value;
 
@@ -63,6 +79,14 @@ public class PropertiesWriter {
             prop.put(CONFIG.OUTPUT_FILE_NAME.name(), CONFIG.OUTPUT_FILE_NAME.toString());
             prop.put(CONFIG.LEVEL.name(), CONFIG.LEVEL.toString());
             prop.put(CONFIG.CHARACTER.name(), CONFIG.CHARACTER.toString());
+            prop.put(CONFIG.EASY_MIN.name(), CONFIG.EASY_MIN.toString());
+            prop.put(CONFIG.MEDIUM_MIN.name(), CONFIG.MEDIUM_MIN.toString());
+            prop.put(CONFIG.HARD_MIN.name(), CONFIG.HARD_MIN.toString());
+            prop.put(CONFIG.CUSTOM_MIN.name(), CONFIG.CUSTOM_MIN.toString());
+            prop.put(CONFIG.EASY_MAX.name(), CONFIG.EASY_MAX.toString());
+            prop.put(CONFIG.MEDIUM_MAX.name(), CONFIG.MEDIUM_MAX.toString());
+            prop.put(CONFIG.HARD_MAX.name(), CONFIG.HARD_MAX.toString());
+            prop.put(CONFIG.CUSTOM_MAX.name(), CONFIG.CUSTOM_MAX.toString());
             prop.store(this.output, null);
         } catch (IOException ioException) {
             Logger.getLogger(PropertiesWriter.class).error("Unable to read file", ioException);
@@ -105,6 +129,29 @@ public class PropertiesWriter {
                 break;
             case CHARACTER:
                 CONFIG.CHARACTER.setValue(value);
+                break;
+            case EASY_MIN:
+                CONFIG.EASY_MIN.setValue(value);
+                break;
+            case MEDIUM_MIN:
+                CONFIG.MEDIUM_MIN.setValue(value);
+                break;
+            case HARD_MIN:
+                CONFIG.HARD_MIN.setValue(value);
+                break;
+            case CUSTOM_MIN:
+                CONFIG.CUSTOM_MIN.setValue(value);
+                break;
+            case EASY_MAX:
+                CONFIG.EASY_MAX.setValue(value);
+            case MEDIUM_MAX:
+                CONFIG.MEDIUM_MAX.setValue(value);
+                break;
+            case HARD_MAX:
+                CONFIG.HARD_MAX.setValue(value);
+                break;
+            case CUSTOM_MAX:
+                CONFIG.CUSTOM_MAX.setValue(value);
                 break;
             default:
                 break;
